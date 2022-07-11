d85e3162d5c2c3c9cb5b3eb15bcce379af31ab4e
progsen <>
2022-07-11 08:43:44 +0200

    initial

---
+++ GameJam/Audio.cs
@@ -0,0 +1,36 @@
+﻿using NAudio.Wave;
+using System;
+
+namespace GameJam
+{
+    public class Audio : IDisposable
+    {
+        private readonly WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        public Audio()
+        {
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+            audioFile = new AudioFileReader(@"test.mp3");
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+        }
+
+        public void Dispose()
+        {
+            outputDevice.Stop();
+            outputDevice.Dispose();
+            audioFile.Dispose();
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            audioFile.Position = 0;
+            outputDevice.Play();
+        }
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gamejam
+
+Simple base template for students to try some game development with
+
+This is pure windows/2d no unity
+This project can also be run on linux when the mono framework has been installed
+
+
+converting to .net 5/6 or core is also possible but then you will loose linux support 
